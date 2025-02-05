05d2167daac2e447f050f413c995f6e00cc37763
Nathan Lecourt <nathan.lecourt@gmail.com>
2025-02-05 10:03:42 +0100

    Comment out unused build steps

--- build.zig
+++ build.zig
@@ -15,19 +15,20 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary(.{
-        .name = "keep-in-touch-backend",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    // This declares intent for the library to be installed into the standard
-    // location when the user invokes the "install" step (the default step when
-    // running `zig build`).
-    b.installArtifact(lib);
+    // root.zig is not used in our project.
+    // const lib = b.addStaticLibrary(.{
+    //     .name = "keep-in-touch-backend",
+    //     // In this case the main source file is merely a path, however, in more
+    //     // complicated build scripts, this could be a generated file.
+    //     .root_source_file = b.path("src/root.zig"),
+    //     .target = target,
+    //     .optimize = optimize,
+    // });
+    //
+    // // This declares intent for the library to be installed into the standard
+    // // location when the user invokes the "install" step (the default step when
+    // // running `zig build`).
+    // b.installArtifact(lib);
 
     const exe = b.addExecutable(.{
         .name = "keep-in-touch-backend",
@@ -64,28 +65,35 @@ pub fn build(b: *std.Build) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    // Creates a step for unit testing. This only builds the test executable
-    // but does not run it.
-    const lib_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
+    // TEST
+    {
+        // Similar to creating the run step earlier, this exposes a `test` step to
+        // the `zig build --help` menu, providing a way for the user to request
+        // running the unit tests.
+        const test_step = b.step("test", "Run unit tests");
+        //
+        // root.zig is not used in our project.
+        // // Creates a step for unit testing. This only builds the test executable
+        // // but does not run it.
+        // const lib_unit_tests = b.addTest(.{
+        //     .root_source_file = b.path("src/root.zig"),
+        //     .target = target,
+        //     .optimize = optimize,
+        // });
+        //
+        // const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
 
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+        const exe_unit_tests = b.addTest(.{
+            // .root_source_file = b.path("src/main.zig"),
+            .root_source_file = b.path("src/tests.zig"),
+            .target = target,
+            .optimize = optimize,
+        });
 
-    const exe_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/tests.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
+        const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
-    test_step.dependOn(&run_exe_unit_tests.step);
+        // root.zig is not used in our project.
+        // test_step.dependOn(&run_lib_unit_tests.step);
+        test_step.dependOn(&run_exe_unit_tests.step);
+    }
 }
